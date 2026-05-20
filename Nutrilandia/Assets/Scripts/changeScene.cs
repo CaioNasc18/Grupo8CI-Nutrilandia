6b9af2a91652b8dad3aef6653965e9fd529720f0
Fl4vinD0Pneu <pv30234@alunos.estgv.ipv.pt>
2026-05-20 11:20:15 +0000

    Revert "Merge branch 'main' of https://github.com/CaioNasc18/Grupo8CI-Nutrilandia"
    This reverts commit 78ffc0b30be6e7afec482aa7e5fd19152df5d1c4, reversing
    changes made to 30b7cdb5480b1763012a308e5f99c0dedf1c678d.

--- Nutrilandia/Assets/Scripts/changeScene.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class ChangeScene : MonoBehaviour
-{
-    public void ChangeSceneAction(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
-}
